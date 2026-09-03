dbe9f7710eb93259572fad1d053ef4ca77facc25
unknown <matchalicei@gmail.com>
2026-09-03 16:07:39 -0300

    pembaruan

--- app/Models/Tarif.php
+++ app/Models/Tarif.php
@@ -7,6 +7,7 @@ use Sakuci\Database\Model;
 class Tarif extends Model
 {
     protected static ?string $table = 'tarif';
+    protected string $primaryKey = 'id_tarif';
 
     protected array $fillable = ['jenis_kendaraan', 'tarif_per_jam'];
 }
