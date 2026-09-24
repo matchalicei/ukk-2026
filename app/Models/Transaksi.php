566527e623a4cb5b3a9e71f168569554ab4bd296
alvira <alvira@sakuci.id>
2026-09-24 14:44:02 +0700

    test

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -7,7 +7,6 @@ use Sakuci\Database\Model;
 class Transaksi extends Model
 {
     protected static ?string $table = 'transaksi';
-     protected string $primaryKey = 'id_transaksi';
 
     protected array $fillable = ['id_parkir', 'id_member', 'waktu_masuk', 'waktu_keluar', 'id_tarif', 'durasi_jam', 'biaya_total', 'status', 'id_user', 'id_area'];
 }
